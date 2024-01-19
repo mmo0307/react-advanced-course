6ea118ef724e9caca4e180b9b5997deaf04f17d2
Mihail <m.misha0307@gmail.com>
2024-01-19 23:37:32 +0200

    change style for component Text & add config to StoreDecorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'feature/AuthByUserName';
 import { DeepPartial } from 'shared/lib/types/DeepPartial';
 
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer
+};
+
 export const StoreDecorator =
-  (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-    <StoreProvider initialState={state}>
+  (
+    state: DeepPartial<StateSchema>,
+    asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
+  ) =>
+  (StoryComponent: Story) => (
+    <StoreProvider
+      initialState={state}
+      asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+    >
       <StoryComponent />
     </StoreProvider>
   );
