6a918ac08392307ad141e6bde4e2c51a42c51b9c
Mihail <m.misha0307@gmail.com>
2024-02-25 02:26:25 +0200

    add comment form

--- src/shared/ui/Page/Page.stories.tsx
+++ src/shared/ui/Page/Page.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Meta, StoryObj } from '@storybook/react';
 
 import { Page } from './Page';
@@ -15,6 +14,6 @@ type Story = StoryObj<typeof meta>;
 
 export const PageStory: Story = {
   args: {
-    children: <span>{useTranslation().t('PageStory')}</span>
+    children: <span />
   }
 };
